e45db7e0ba7e447be6f2a341a1065725f77d3935
[jeradcliffe] <jacobradcliffe2012@gmail.com>
2017-09-10 14:02:35 -0500

    #1 DBContext class added.

---
+++ SpectrumDemoRadcliffe/Data/FileContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpectrumDemoRadcliffe.Models;
+
+namespace SpectrumDemoRadcliffe.Data
+{
+    public class FileContext : DbContext
+    {
+        public FileContext(DbContextOptions<FileContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<File> Files{ get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<File>().ToTable("Files");
+            modelBuilder.Entity<Person>().ToTable("Persons");
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+    }
+
+
+
+}
